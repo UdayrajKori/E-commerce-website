15b5d64c5085d702aaa157ae60cd703ca60fd16a
Aiko14510 <aasthasherchan78699@gmail.com>
2025-05-22 13:16:51 +0545

    change the add to cart button color to test

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -29,7 +29,7 @@ const ProductCard = ({ price, image, title, description, category, id}) => {
           {description}
         </p>
 
-        <button className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+        <button className="px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
           Add to Cart
         </button>
       </div>
